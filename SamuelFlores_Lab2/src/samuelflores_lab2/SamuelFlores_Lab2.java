22daf5483e1ea941b03e7245fc9317efa3cf15cd
SamuelFlo <samuelflores100@hotmail.com>
2018-02-02 15:38:10 -0600

    llevo parte de log in y parte de eliminar

--- SamuelFlores_Lab2/src/samuelflores_lab2/SamuelFlores_Lab2.java
+++ SamuelFlores_Lab2/src/samuelflores_lab2/SamuelFlores_Lab2.java
@@ -13,7 +13,11 @@ import javax.swing.JOptionPane;
  * @author Samuel
  */
 public class SamuelFlores_Lab2 {
-    static ArrayList detectives = new ArrayList();
+    static ArrayList<detectives> detectives = new ArrayList();
+    static ArrayList casos = new ArrayList();
+    static detectives log;
+    static String usuario="";
+    static String contrasena="";
 
     /**
      * @param args the command line arguments
@@ -33,8 +37,18 @@ public class SamuelFlores_Lab2 {
                 modificar();
                 
             }else if(opcion.equalsIgnoreCase("c")){
+                eliminar();
                 
             }else if(opcion.equalsIgnoreCase("d")){
+                usuario=JOptionPane.showInputDialog("Ingrese su usuario: ");
+                contrasena=JOptionPane.showInputDialog("Ingrese su contrasena: ");
+                for (detectives usuario : detectives) {
+                    if(usuario.getUsuario().equals(usuario)&&usuario.getContrasena().equals(contrasena)){
+                        System.out.println("BIENVENIDO");
+                    }
+                    
+                }
+                
                 
             }else if(opcion.equalsIgnoreCase("e")){
                 
@@ -83,10 +97,41 @@ public class SamuelFlores_Lab2 {
             int nivel = Integer.parseInt(niv);
             String listadecasos = "";
             String buzon = JOptionPane.showInputDialog("Ingrese su mensaje en el buzon: ");
-            String usuario = JOptionPane.showInputDialog("Ingrese su usuario: ");
-            String contrasena = JOptionPane.showInputDialog("Ingrese su contrasena: ");
+            usuario = JOptionPane.showInputDialog("Ingrese su usuario: ");
+            contrasena = JOptionPane.showInputDialog("Ingrese su contrasena: ");
             detectives.set(pos, new detectives(nombre, edad, nacionalidad, anoslaborales, nivel, listadecasos, buzon, usuario, contrasena));
         }
 
     }
+    public static void eliminar(){
+        String salida = "";
+        for (Object temp : detectives) {
+
+            if (temp instanceof detectives) {
+                salida += detectives.indexOf(temp) + " ";
+                salida += temp + "\n";
+            }
+        }
+        JOptionPane.showMessageDialog(null, salida);
+        String pos1 = JOptionPane.showInputDialog("Ingrese la posicion que desea modificar: ");
+        int pos = Integer.parseInt(pos1);
+        if (pos >= 0 && pos < detectives.size() && detectives.get(pos) instanceof detectives) {
+            detectives.remove(pos);
+        }
+        
+    }
+    public static void login(){
+        
+    }
+    
+    public static boolean contra(String pas, String user){
+        for (detectives usuario : detectives) {
+            if(usuario.getContrasena().equals(pas)&&usuario.getUsuario().equals(user)){
+                log=usuario;
+                return true;
+            }
+        }
+        return false;
+    }
+    
 }
---
+++ SamuelFlores_Lab2/src/samuelflores_lab2/login.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package samuelflores_lab2;
+
+/**
+ *
+ * @author Samuel
+ */
+public class login {
+    private String lugar;
+    private String tipo;
+    private String descripcion;
+    private String detectiveacargo;
+    private String estado;
+
+    public login() {
+    }
+
+    public login(String lugar, String tipo, String descripcion, String detectiveacargo, String estado) {
+        this.lugar = lugar;
+        this.tipo = tipo;
+        this.descripcion = descripcion;
+        this.detectiveacargo = detectiveacargo;
+        this.estado = estado;
+    }
+
+    public String getLugar() {
+        return lugar;
+    }
+
+    public void setLugar(String lugar) {
+        this.lugar = lugar;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDetectiveacargo() {
+        return detectiveacargo;
+    }
+
+    public void setDetectiveacargo(String detectiveacargo) {
+        this.detectiveacargo = detectiveacargo;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "lugar = "+lugar+"tipo = "+tipo+"descripcion = "+descripcion+"detectiveacargo = "+detectiveacargo+"estado = "+estado; //To change body of generated methods, choose Tools | Templates.
+    }
+}
