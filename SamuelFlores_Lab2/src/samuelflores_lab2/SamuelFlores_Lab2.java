9ee18c578434428fee9f223a53c852378dd03112
SamuelFlo <samuelflores100@hotmail.com>
2018-02-02 14:31:56 -0600

    aqui ya termine agregar y modificar

---
+++ SamuelFlores_Lab2/src/samuelflores_lab2/SamuelFlores_Lab2.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package samuelflores_lab2;
+
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Samuel
+ */
+public class SamuelFlores_Lab2 {
+    static ArrayList detectives = new ArrayList();
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String opcion = "";
+        while (!opcion.equalsIgnoreCase("e")) {
+            opcion = JOptionPane.showInputDialog("a-Agregar detective\n"
+                    + "b-Modificar detectives\n"
+                    + "c-Eliminar detectives\n"
+                    + "d-Log in\n"
+                    + "e-Salir");
+            if(opcion.equalsIgnoreCase("a")){
+                agregar();
+            }else if(opcion.equalsIgnoreCase("b")){
+                modificar();
+                
+            }else if(opcion.equalsIgnoreCase("c")){
+                
+            }else if(opcion.equalsIgnoreCase("d")){
+                
+            }else if(opcion.equalsIgnoreCase("e")){
+                
+            }else if(opcion.equals("f")){
+                
+            }
+        }
+        
+
+    }
+    public static void agregar(){
+        String nombre = JOptionPane.showInputDialog("Ingrese su nombre: ");
+        String eda = JOptionPane.showInputDialog("Ingrese su edad: ");
+        int edad=Integer.parseInt(eda);
+        String nacionalidad = JOptionPane.showInputDialog("Ingrese su nacionalidad: ");
+        String anoslab = JOptionPane.showInputDialog("Ingrese sus anos laborales: ");
+        int anoslaborales=Integer.parseInt(anoslab);
+        String niv = JOptionPane.showInputDialog("Ingrese el nivel: ");
+        int nivel=Integer.parseInt(niv);
+        String listadecasos="";
+        String buzon = JOptionPane.showInputDialog("Ingrese su mensaje en el buzon: ");
+        String usuario = JOptionPane.showInputDialog("Ingrese su usuario: ");
+        String contrasena = JOptionPane.showInputDialog("Ingrese su contrasena: ");
+        detectives.add(new detectives(nombre, edad, nacionalidad, anoslaborales, nivel, listadecasos, buzon, usuario, contrasena));
+    }
+    public static void modificar() {
+        String salida = "";
+        for (Object temp : detectives) {
+
+            if (temp instanceof detectives) {
+                salida += detectives.indexOf(temp) + " ";
+                salida += temp + "\n";
+            }
+        }
+        JOptionPane.showMessageDialog(null, salida);
+        String pos1 = JOptionPane.showInputDialog("Ingrese la posicion que desea modificar: ");
+        int pos = Integer.parseInt(pos1);
+        if (pos >= 0 && pos < detectives.size() && detectives.get(pos) instanceof detectives) {
+            String nombre = JOptionPane.showInputDialog("Ingrese su nombre: ");
+            String eda = JOptionPane.showInputDialog("Ingrese su edad: ");
+            int edad = Integer.parseInt(eda);
+            String nacionalidad = JOptionPane.showInputDialog("Ingrese su nacionalidad: ");
+            String anoslab = JOptionPane.showInputDialog("Ingrese sus anos laborales: ");
+            int anoslaborales = Integer.parseInt(anoslab);
+            String niv = JOptionPane.showInputDialog("Ingrese el nivel: ");
+            int nivel = Integer.parseInt(niv);
+            String listadecasos = "";
+            String buzon = JOptionPane.showInputDialog("Ingrese su mensaje en el buzon: ");
+            String usuario = JOptionPane.showInputDialog("Ingrese su usuario: ");
+            String contrasena = JOptionPane.showInputDialog("Ingrese su contrasena: ");
+            detectives.set(pos, new detectives(nombre, edad, nacionalidad, anoslaborales, nivel, listadecasos, buzon, usuario, contrasena));
+        }
+
+    }
+}
---
+++ SamuelFlores_Lab2/src/samuelflores_lab2/detectives.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package samuelflores_lab2;
+
+/**
+ *
+ * @author Samuel
+ */
+public class detectives {
+    private String nombre;
+    private int edad;
+    private String nacionalidad;
+    private int anoslaborales;
+    private int nivel;
+    private String listadecasos;
+    private String buzon;
+    private String usuario;
+    private String contrasena;
+
+    public detectives() {
+    }
+
+    public detectives(String nombre, int edad, String nacionalidad, int anoslaborales, int nivel, String listadecasos, String buzon, String usuario, String contrasena) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.nacionalidad = nacionalidad;
+        this.anoslaborales = anoslaborales;
+        this.nivel = nivel;
+        this.listadecasos = listadecasos;
+        this.buzon = buzon;
+        this.usuario = usuario;
+        this.contrasena = contrasena;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public int getAnoslaborales() {
+        return anoslaborales;
+    }
+
+    public void setAnoslaborales(int anoslaborales) {
+        this.anoslaborales = anoslaborales;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    public String getListadecasos() {
+        return listadecasos;
+    }
+
+    public void setListadecasos(String listadecasos) {
+        this.listadecasos = listadecasos;
+    }
+
+    public String getBuzon() {
+        return buzon;
+    }
+
+    public void setBuzon(String buzon) {
+        this.buzon = buzon;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    @Override
+    public String toString() {
+        return " nombre= "+nombre+" edad= "+edad+" nacionalidad= "+nacionalidad; //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+    
+}
